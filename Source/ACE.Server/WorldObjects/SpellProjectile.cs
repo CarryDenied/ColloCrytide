b9d03d30e7f850e0d57e6cf5405b6ef20c58394d
Fudly <fake@example.com>
2023-03-24 18:28:37 -0600

    Buffed mage damage back to where it was originally (because I nerfed slayer in a previous commit)

--- Source/ACE.Server/WorldObjects/SpellProjectile.cs
+++ Source/ACE.Server/WorldObjects/SpellProjectile.cs
@@ -617,7 +617,7 @@ namespace ACE.Server.WorldObjects
                 {
                     if (Common.ConfigManager.Config.Server.WorldRuleset == Common.Ruleset.CustomDM)
                     {
-                        finalDamage += (int)((float)finalDamage * .2);
+                        finalDamage += (int)((float)finalDamage * .35);
                     }
                 }
             }
