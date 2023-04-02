105cb8e0f9a6e1ea49d947f475ea0b782e64e466
Michael Rath <mikerath@gmail.com>
2023-04-01 21:42:17 -0700

    Extra configs for tweaking shield cleave and pvp AL/SL reduction

--- Source/ACE.Server/Entity/DamageEvent.cs
+++ Source/ACE.Server/Entity/DamageEvent.cs
@@ -378,8 +378,12 @@ namespace ACE.Server.Entity
 
             var ignoreArmorMod = Math.Min(armorRendingMod, armorCleavingMod);
 
-            //if (Common.ConfigManager.Config.Server.WorldRuleset == Common.Ruleset.CustomDM && pkBattle)
-            //    ignoreArmorMod *= 0.7f; // Armor is reduced during PvP.
+            if (Common.ConfigManager.Config.Server.WorldRuleset == Common.Ruleset.CustomDM && pkBattle)
+            {
+                ignoreArmorMod *= (float)PropertyManager.GetDouble("pvp_dmg_armor_base_reduction").Item; // Armor is reduced during PvP.
+            }
+
+            
 
             // get body part / armor pieces / armor modifier
             if (playerDefender != null)
@@ -718,6 +722,9 @@ namespace ACE.Server.Entity
                                     break;
                             }
                         }
+
+                        if(SneakAttackMod > 1.0)
+                            config_mod *= (float)PropertyManager.GetDouble("pvp_dmg_sneak_mod").Item;
 
                         Damage = Damage * config_mod;
                     }
