79cc1ae9fb8ae4dfb07cf0f60eeac056325a0aca
Advan <126741458+xAdvan@users.noreply.github.com>
2024-01-10 13:25:46 -0800

    Add Colo Content
    - Add colo content, weenies, quests, spells, landblocks, events
    Note: Events will need to be imported via DB as far as I can tell.

---
+++ Source/ACE.Server/Content/sql/landblocks/00B6.sql
@@ -0,0 +1,13 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0x00B6;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x700B6000, 71053, 0x00B6010D, 29.7183, -180.113, -11.89, 0.0352889, 0, 0, -0.999377, False, '2020-05-22 22:58:24'); /* Killagurg Event Watcher */
+/* @teleloc 0x00B6010D [29.718300 -180.113007 -11.890000] 0.035289 0.000000 0.000000 -0.999377 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x700B6001, 71050, 0x00B60104, 24.1407, -180.041, -11.839, 0.010296, 0, 0, -0.999947, False, '2020-05-22 22:58:46'); /* killagurgGen1 */
+/* @teleloc 0x00B60104 [24.140699 -180.041000 -11.839000] 0.010296 0.000000 0.000000 -0.999947 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x700B6002, 71051, 0x00B60116, 35.7158, -179.80247, -11.839, 0.0102959825, 0, 0, -0.999947, False, '2020-05-22 22:58:53'); /* killagurgBossGen */
+/* @teleloc 0x00B60116 [35.715801 -179.802475 -11.839000] 0.010296 0.000000 0.000000 -0.999947 */
